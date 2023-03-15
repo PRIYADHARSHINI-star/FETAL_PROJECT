693bb95e25a6ecc427a552f5d6d91ade034e2e3c
Priyadharshini R <72924709+PRIYADHARSHINI-star@users.noreply.github.com>
2023-03-15 21:02:21 +0530

    Delete authentication directory

--- authentication/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Register, contact1, User_Detail
-# Register your models here.
-
-admin.site.register(Register)
-admin.site.register(contact1)
-admin.site.register(User_Detail)
